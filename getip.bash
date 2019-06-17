c246798480474baef07c8535507f49742286ddcd
alexandrianlibrarian <alexandrian.librarian1@gmail.com>
2019-06-17 11:18:33 +0100

    Implemented checking.
    Updated code to ensure getip.bash does not pass empty string as output.

--- getip.bash
+++ getip.bash
@@ -1,2 +1,14 @@
 #!/bin/bash
-dig +short myip.opendns.com @resolver1.opendns.com
+
+# Funciton to get public ip and assign it to variable "ip".
+getIP () {
+        ip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+}
+
+# Ensuring we don't get an empty IP as output.
+getIP
+until [[ -n "$ip"  ]]; do
+        getIP
+done
+echo "$ip"
+
